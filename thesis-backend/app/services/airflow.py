ece52ac4d65ab7e64a1bc91a578880720975a22c
TasosK7 <a.kanellias03@gmail.com>
2025-07-20 17:46:08 +0300

    Changed env variables for airlfow

--- thesis-backend/app/services/airflow.py
+++ thesis-backend/app/services/airflow.py
@@ -1,13 +1,13 @@
 import requests
 import os
 
-AIRFLOW_DAG_TRIGGER_URL = os.getenv("AIRFLOW_DAG_TRIGGER_URL", "http://localhost:8080/api/v1/dags/appointment_email_flow/dagRuns")
+AIRFLOW_EMAIL_DAG_TRIGGER_URL = os.getenv("AIRFLOW_DAG_TRIGGER_URL", "http://localhost:8080/api/v1/dags/appointment_email_flow/dagRuns")
 AIRFLOW_USERNAME = os.getenv("AIRFLOW_USERNAME", "airflow")
 AIRFLOW_PASSWORD = os.getenv("AIRFLOW_PASSWORD", "airflow")
 
 def trigger_appointment_dag(appointment_id: int):
     response = requests.post(
-        AIRFLOW_DAG_TRIGGER_URL,
+        AIRFLOW_EMAIL_DAG_TRIGGER_URL,
         auth=(AIRFLOW_USERNAME, AIRFLOW_PASSWORD),
         json={"conf": {"appointment_id": appointment_id}},
         headers={"Content-Type": "application/json"},
